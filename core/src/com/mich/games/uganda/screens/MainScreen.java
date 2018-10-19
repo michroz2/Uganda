2c85ca96049a139756ac1e684ebf41f185a446ba
andrei-prokofjev <andrei.prokofjev@gmail.com>
2018-10-19 16:06:31 +0300

    resources moved into root directory

--- core/src/com/mich/games/uganda/screens/MainScreen.java
+++ core/src/com/mich/games/uganda/screens/MainScreen.java
@@ -1,5 +1,8 @@
 package com.mich.games.uganda.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.mich.common.gdx.screen.GenericScreen;
 
 public class MainScreen extends GenericScreen {
@@ -12,6 +15,14 @@ public class MainScreen extends GenericScreen {
     @Override
     public void show() {
         super.show();
+        System.out.println(">show");
+
+        Texture texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+
+        System.out.println(">>> t: " + texture);
+        Image img = new Image(texture);
+        addActor(img);
+
 
 
     }
