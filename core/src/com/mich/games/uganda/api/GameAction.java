cf5f655c7ea612244e34153aae7f00cb1c5c9141
michroz2 <michroz@gmail.com>
2018-10-23 01:07:08 +0300

    GameLogic - draft still in progress...

--- core/src/com/mich/games/uganda/api/GameAction.java
+++ core/src/com/mich/games/uganda/api/GameAction.java
@@ -1,7 +1,10 @@
 package com.mich.games.uganda.api;
 
 
+import java.util.Random;
+
 public class GameAction {
+    private static final String TAG = "GameAction: ";
 
     int level;
     int[] sequenceSize;
@@ -10,6 +13,8 @@ public class GameAction {
     int playfieldSize;
     int correctAnswer;
     int[][] sequence;
+    int[][] mainSequence;
+    int mainSequenceSize;
     int[][] playingSequence;
     int[] points;
     int currentElement;
@@ -26,8 +31,57 @@ public class GameAction {
 
     }
 
-    void generateLevel(int level) {
+    void createLevel(int level) {
+        numSequences = getNumSequencesByLevel(level);
+        sequenceSize = new int[numSequences];
+        for (int i = 0; i < numSequences; i++) {
+            sequenceSize[i] = getSequenceSizeByLevel(level, i);
+        }
+        mainSequenceSize = calcMainSequenceSize(sequenceSize);
+        sequence = new int[numSequences][mainSequenceSize];
+        for (int i = 0; i < numSequences; i++) {
+            sequence[i] = createSingleSequence(sequenceSize[i], mainSequenceSize);
+        }
+        int a = 10;
+    }
 
+    int calcMainSequenceSize(int array[]) { // returns lcm of the array of int values
+        int result = 1;
+        for (int i = 0; i < array.length; i++) {
+            result = lcm(result, array[i]);
+        }
+        return result;
+    }
+
+    int gcd(int a, int b) { // greatest common divider
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    int lcm(int a, int b) { // least common multiple
+        return a / gcd(a, b) * b;
+    }
+
+    /**
+     * @param size
+     * @return случайно перемешанную последовательность чисел от 0 до size-1
+     */
+    int[] createSingleSequence(int size, int mainSize) {
+        int[] result = new int[mainSize];
+        Random rand = new Random();
+
+        for (int i = 0; i < size; i++) { // заполняем последовательно массив (можно и НЕ последовательно, на будущее...)
+            result[i] = i;
+        }
+        for (int i = 0; i < size; i++) { // shuffle randomly
+            int j = rand.nextInt(size);
+            int temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+        }
+        for (int i = 0; i < mainSize; i++) {  // fill with same pattern
+            result[i] = result[i % size];
+        }
+        return result;
     }
 
     /**
@@ -44,7 +98,6 @@ public class GameAction {
      * @return задаёт длину конкретной отдельной последовательности исходя из уровня
      */
     int getSequenceSizeByLevel(int level, int sequenceNumber) {
-        int minSize = 3;
         return (level % 5) + (level / 5) + 1 + sequenceNumber;
     }
 
--- core/test/com/mich/games/uganda/api/GameActionTest.java
+++ core/test/com/mich/games/uganda/api/GameActionTest.java
@@ -2,17 +2,33 @@ package com.mich.games.uganda.api;
 
 import junit.framework.TestCase;
 
+import org.junit.Assert;
+
 public class GameActionTest extends TestCase {
 
 
     public void testStartGame() {
         GameAction gameAction = new GameAction();
 
+        int a = gameAction.gcd(40, 16);
+        Assert.assertTrue(a == 8);
+
+        gameAction.createLevel(1);
+        gameAction.createLevel(2);
+        gameAction.createLevel(3);
+        gameAction.createLevel(6);
+        gameAction.createLevel(8);
+        gameAction.createLevel(12);
+        gameAction.createLevel(15);
+
+
+/*
         gameAction.startGame();
         gameAction.makeMove(555);
         gameAction.makeMove(666);
         gameAction.makeMove(222);
 
+*/
 
     }
 }
